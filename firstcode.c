2671fd2358abb6f4e4ff72143182994493266b2d
ajaykumar0506 <b.kumar2@s.amity.edu>
2025-05-07 00:45:35 +0530

    C add sub program completed

--- firstcode.c
+++ firstcode.c
@@ -1,13 +1,12 @@
-// add_sub_part1.c - Version 2
+// add_sub_part1.c - Version 3
 #include <stdio.h>
 
 int main() {
+    // 'int' is used for integer values
     int a, b;
-    // Taking input
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
     int sum = a + b;
-    // Displaying sum
     printf("Sum: %d\n", sum);
 
