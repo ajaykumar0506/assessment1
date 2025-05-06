8dd11fde0ffa10d4cb1465e14a1a228e6108e266
ajaykumar0506 <b.kumar2@s.amity.edu>
2025-05-07 00:23:00 +0530

    C add sub program

---
+++ firstcode.c
@@ -0,0 +1,11 @@
+// add_sub_part1.c - Version 1
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int sum = a + b;
+    printf("Sum: %d\n", sum);
+
